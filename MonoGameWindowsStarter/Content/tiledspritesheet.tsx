83aab77e7b7553b4646ddac2b1e3551b2632b40a
Kevin Nolde <kedlon@ksu.edu>
2020-03-29 22:34:40 -0500

    Included ContentImporter and Platform Library from previous examples.

---
+++ MonoGameWindowsStarter/Content/tiledspritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="spritesheet" tilewidth="21" tileheight="21" tilecount="561" columns="33">
+ <image source="spritesheet.png" width="694" height="372"/>
+</tileset>
